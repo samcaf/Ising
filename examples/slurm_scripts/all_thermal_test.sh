5679ceffa0c3c42c1af1a55a4eeb3ab80526e7c4
samcaf <samuelaf@mit.edu>
2022-01-24 22:25:21 -0500

    Changed syntax of script for slurm scripts.

---
+++ examples/slurm_scripts/all_thermal_test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name all_thermal_test
+#SBATCH -o logs/all_thermal_test-%j.out
+
+set -e
+echo "# ========================
+# Performing Ising Tests
+# ========================
+Start time: "
+date
+echo "
+
+# ------------------
+Saving models
+# ------------------
+Starting at "
+date
+sbatch -W examples/slurm_scripts/save_models.sh
+echo "Complete at "
+date
+echo "
+
+# ------------------
+Plotting Basic Tests
+# ------------------
+Starting at "
+date
+sbatch -W examples/slurm_scripts/test_thermal_basic.sh
+echo "Complete at"
+date
+echo "
+
+
+"
+wait
+
+echo "End time: "
+date
+echo "
+
+All tasks complete."
